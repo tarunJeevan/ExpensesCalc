246ab3563f8464d75c21af11c4c14c16b1a63d4d
Tarun Jeevan <tjeevan200@gmail.com>
2023-06-20 18:37:59 -0500

    Project setup

---
+++ ExpensesCalc/main.cpp
@@ -0,0 +1,9 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!" << std::endl;
+}
